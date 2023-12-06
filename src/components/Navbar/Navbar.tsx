29b6fe9339c04bbf6327aa3a3d7d00ae58dd659d
unknown <jschueren1431@gmail.com>
2023-12-05 17:44:22 -0800

    fixed an error when when user has no profile picture

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar: React.FC<NavbarProps> = (props) => {
     let picture = (<svg className="nav-profile" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 340 340">
                     <path fill="#DDD" d="m169,.5a169,169 0 1,0 2,0zm0,86a76,76 0 1 1-2,0zM57,287q27-35 67-35h92q40,0 67,35a164,164 0 0,1-226,0"/>
                 </svg>)
-    if(props.image !== undefined){
+    if(props.image){
         const uint8Array = new Uint8Array(props.image.data);
         const blob = new Blob([uint8Array], { type: "buffer" });
         picture = (<img className="profile-pic"src={URL.createObjectURL(blob)}/>)
